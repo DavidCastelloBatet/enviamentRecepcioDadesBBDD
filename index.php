211905615ee93ea1ad2fd1b2f42e5aeac624a1d6
David <davidcastellobatet@gmail.com>
2022-05-18 10:11:56 +0200

    commit inicial

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conexió i Enviament Dades a BBDD MySQL</title>
+</head>
+<body>
+  <h1>Conexió i Enviament de Dades a BBDD MySQL</h1>
+
+  <form method="GET" action="index.php">
+    <input type="text" name="nomNecessariDeLaDada" >
+    <input type="submit">
+	</form>
+
+</body>
+</html>
